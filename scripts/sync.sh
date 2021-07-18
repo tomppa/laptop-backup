543f303c97f08099301b6b9b6670e0e3553efd24
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-07-18 23:41:21 +0300

    Finetuned the script, moved configuration to a properties file, and added functionality to read it from there.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,22 +1,71 @@
 #!/bin/bash
 
-set -x
+DEBUG="${1:-''}"
+CONFIG_FOLDER='../config'
+PROP_FILE='configuration.properties'
 
-DRYRUN=true
+# properties is an associated array, i.e. keys can be strings or variables
+declare -A properties
 
-s3_bucket_uri='s3://laptopbackupstack-backupbucket87437600-1t7jods4leuhe'
-aws_profile='--profile=personal'
-backup_config_folder='../config/backup/*'
-include_file='includes.txt'
-exclude_file='excludes.txt'
+# includes and excludes are Bash arrays, i.e., with auto-numbered keys
+declare -a includes
+declare -a excludes
 
-includes=()
-excludes=()
+function loadProperties {
+    local file="$CONFIG_FOLDER/$PROP_FILE"
+    if [[ ! -f "$file" ]]; then
+        echo "$PROP_FILE not found!"
+        return 2
+    fi
 
-sync () {
+    while IFS='=' read -r origKey value; do
+        local key="$origKey"
+        # Replace all non alphanumerical characters (except underscore)
+        # with an underscore
+        key="${key//[!a-zA-Z0-9_]/_}"
+
+        if [[ "$origKey" == "#"* ]]; then
+            local ignoreComments
+        elif [[ -z "$key" ]]; then
+            local emptyLine
+        else
+            properties["$key"]="$value"
+        fi
+    done < "$file"
+
+    if [[ "$DEBUG" == "--debug" ]]; then
+        declare -p properties
+    fi
+}
+
+function checkBucket {
+    local params=()
+    params+=(--bucket "${properties[s3_bucket]}")
+    params+=(--profile="${properties[aws_profile]}")
+    local bucketStatus=$(aws s3api head-bucket ${params[@]} 2>&1)
+    
+    if [[ -z "$bucketStatus" ]]; then
+        echo "Bucket \"${properties[s3_bucket]}\" owned and exists";
+        return 0
+    elif echo "${bucketStatus}" | grep 'Invalid bucket name'; then
+        return 1
+    elif echo "${bucketStatus}" | grep 'Not Found'; then
+        return 1
+    elif echo "${bucketStatus}" | grep 'Forbidden'; then
+        echo "Bucket exists but not owned"
+        return 1
+    elif echo "${bucketStatus}" | grep 'Bad Request'; then
+        echo "Bucket name specified is less than 3 or greater than 63 characters"
+        return 1
+    else
+        return 1
+    fi
+}
+
+function sync {
     local params=()
     local local_folder="$HOME/$1"
-    local bucket_folder="$s3_bucket_uri""$local_folder"
+    local bucket_folder="s3://${properties[s3_bucket]}$local_folder"
 
     params+=("$local_folder" "$bucket_folder")
 
@@ -28,16 +77,16 @@ sync () {
         params+=("${includes[@]}")
     fi
 
-    params+=("$aws_profile")
+    params+=("--profile=${properties[aws_profile]}")
 
-    if [[ "$DRYRUN" = true ]]; then
+    if [[ "${properties[dryrun]}" = true ]]; then
         params+=(--dryrun)
     fi
 
     aws s3 sync "${params[@]}"
 }
 
-read_parameters () {
+function read_parameters {
     if [[ -f "$1" ]]; then
         while read line; do
             if [[ $2 == "include" ]]; then
@@ -49,15 +98,33 @@ read_parameters () {
     fi
 }
 
-reset () {
-    includes=()
-    excludes=()
+function reset {
+    unset includes excludes
+    declare -a includes excludes
 }
+
+if [[ "$DEBUG" == "--debug" ]]; then
+    set -x
+fi
+
+loadProperties
+
+if [[ $? != 0 ]]; then
+    exit
+fi
+
+checkBucket
+
+if [[ $? != 0 ]]; then
+    exit
+fi
+
+backup_config_folder="$CONFIG_FOLDER/${properties[backup_folder]}"
 
 for folder in $backup_config_folder; do
     if [[ -d "$folder" && ! -L "$folder" ]]; then
-        read_parameters $folder/$exclude_file exclude
-        read_parameters $folder/$include_file include
+        read_parameters "$folder/${properties[exclude_file_name]}" exclude
+        read_parameters "$folder/${properties[include_file_name]}" include
         sync "${folder##*/}"
         reset
     fi
